45052f484dbca32fe99887a12160b02cac219a14
Jinwon Seo <jwseo@live.co.kr>
2023-08-17 16:33:51 +0900

    Add test.

---
+++ src/test/java/kr/sprouts/autoconfigure/security/web/application/mock/MockPermitAllTestController.java
@@ -0,0 +1,46 @@
+package kr.sprouts.autoconfigure.security.web.application.mock;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = MockPermitAllTestController.REQUEST_PATH)
+public class MockPermitAllTestController {
+    static final String REQUEST_PATH = "/mock/permit-all";
+    static final String BODY = "permitAll";
+    @GetMapping
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok(BODY);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> post() {
+        return ResponseEntity.ok(BODY);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> put() {
+        return ResponseEntity.ok(BODY);
+    }
+
+    @PatchMapping
+    public ResponseEntity<String> patch() {
+        return ResponseEntity.ok(BODY);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> delete() {
+        return ResponseEntity.ok(BODY);
+    }
+
+    @GetMapping(value = "/ext")
+    public ResponseEntity<String> getExt() {
+        return ResponseEntity.ok(BODY);
+    }
+}
